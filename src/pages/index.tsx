7fd006c58891ba27488210a35334a75872dd737e
NextDevApp <appdevjack@outlook.com>
2022-04-25 23:54:19 -0700

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from "next";
 import { FC } from "react";
-import ReactPlayer from "react-player";
 import StorefrontLayout from "../components/layouts/StorefrontLayout";
 import FeatureProducts from "../components/products/FeatureProducts";
 
 const Home = () => {
-  return <div>
-    
-    </div>;
+  return <div>Home</div>;
 };
 
 Home.layout = StorefrontLayout;
